28f72b2b328a82b05ea3064291683e1352df70e5
stackviolator <joshmerrill255@gmail.com>
2025-01-06 14:57:44 -0600

    add IAgent interface

--- redcon/main.py
+++ redcon/main.py
@@ -1,8 +1,9 @@
-from agent import Agent
+from redcon.agents.ReasoningAgent import ReasoningAgent
 
 if __name__ == "__main__":
     # model = "llama3.1:8b" # damn this thing is stupid
     model = "gpt-4o"
-    agent = Agent(model=model)
-    # agent.run_nmap_scan({"args":"-p 88,135-139,445 -sVC -oA scans/nmap/domainControllers","outfile":"scans/nmap/domainControllers.xml","target":"172.16.29.0/24"})
-    agent.run()
+    agent = ReasoningAgent(model=model)
+    # agent.run_nmap_scan({"args":"ballsack lol"})
+    print(agent.rag_query({"query":"Domain controller recon techniques"}))
+    # agent.run()
